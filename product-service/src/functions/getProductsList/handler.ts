88c57124837f6141a84dffd48194c31d3ed539f2
Alex <itgtktdbx@gmail.com>
2022-09-25 15:59:19 +0300

    test: cover lambda handlers with unit tests

--- product-service/src/functions/getProductsList/handler.ts
+++ product-service/src/functions/getProductsList/handler.ts
@@ -1,10 +1,14 @@
 import { formatJSONResponse, ValidatedEventAPIGatewayProxyEvent } from "@libs/api-gateway";
 import { middyfy } from "@libs/lambda";
 import { getProductRepository } from "@repositories/product";
+import { ProductRepository } from "@repositories/product/types";
 import schema from "./schema";
 
-const getProductsList: ValidatedEventAPIGatewayProxyEvent<typeof schema> =async () => {
-  return formatJSONResponse(await getProductRepository().getAll());
+export function createHandler(getProductRepository: () => ProductRepository) {
+  const getProductsList: ValidatedEventAPIGatewayProxyEvent<typeof schema> =async () => {
+    return formatJSONResponse(await getProductRepository().getAll());
+  }
+  return getProductsList;
 }
 
-export const main = middyfy(getProductsList);
+export const main = middyfy(createHandler(getProductRepository));
