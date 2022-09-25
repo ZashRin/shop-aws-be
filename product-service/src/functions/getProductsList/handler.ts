e1891d898a431bd844fcaee9cdbb451b1177d9c4
Alex <itgtktdbx@gmail.com>
2022-09-25 15:03:16 +0300

    feat: add product service functions

---
+++ product-service/src/functions/getProductsList/handler.ts
@@ -0,0 +1,10 @@
+import { formatJSONResponse, ValidatedEventAPIGatewayProxyEvent } from "@libs/api-gateway";
+import { middyfy } from "@libs/lambda";
+import { getProductRepository } from "@repositories/product";
+import schema from "./schema";
+
+const getProductsList: ValidatedEventAPIGatewayProxyEvent<typeof schema> =async () => {
+  return formatJSONResponse(await getProductRepository().getAll());
+}
+
+export const main = middyfy(getProductsList);
