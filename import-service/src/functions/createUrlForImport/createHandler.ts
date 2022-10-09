a3aaa38ae6152de45e6e03aa29368c6f9903edcc
Alex <itgtktdbx@gmail.com>
2022-10-09 13:42:19 +0300

    fix: file name parameter

--- import-service/src/functions/createUrlForImport/createHandler.ts
+++ import-service/src/functions/createUrlForImport/createHandler.ts
@@ -18,7 +18,7 @@ export function createHandler({
 		typeof schema
 	> = async (event) => {
 		try {
-			const fileName = event.queryStringParameters.filename;
+			const fileName = event.queryStringParameters.name;
 
 			const s3Instance = getS3Instance();
 
