61f6f17f7fa97c6a6eaf2397d443e7ddf71c7bc5
Alex <itgtktdbx@gmail.com>
2022-10-16 14:58:52 +0300

    feat: extract lambdas from vpc

--- product-service/src/functions/catalogBatchProcess/createHandler.ts
+++ product-service/src/functions/catalogBatchProcess/createHandler.ts
@@ -6,56 +6,49 @@ import { Logger } from "@libs/logger/types";
 import { ProductRepository } from "@repositories/product/types";
 
 export function createHandler({
-	getProductRepository,
-	getSns,
-	getLogger,
-	topicArn,
+  getProductRepository,
+  getSns,
+  getLogger,
+  topicArn,
 }: {
-	getProductRepository: () => ProductRepository;
-	getSns: () => SNS;
-	topicArn: string;
-	getLogger: () => Logger;
+  getProductRepository: () => ProductRepository;
+  getSns: () => SNS;
+  topicArn: string;
+  getLogger: () => Logger;
 }) {
-	const createProduct = async (event: SQSEvent) => {
-		try {
-			const logger = getLogger();
-			logger.log(event.Records as unknown as Record<string, unknown>);
+  const createProduct = async (event: SQSEvent) => {
+    try {
+      const logger = getLogger();
+      logger.log(event.Records as unknown as Record<string, unknown>);
 
-			const productsToCreate = event.Records.map(({ body }) =>
-				JSON.parse(body)
-			);
+      const productsToCreate = event.Records.map(({ body }) =>
+        JSON.parse(body)
+      );
 
-			await getProductRepository().createBatch(productsToCreate);
+      await getProductRepository().createBatch(productsToCreate);
 
-			const message = JSON.stringify(productsToCreate);
+      await getSns()
+        .publish({
+          TopicArn: topicArn,
+          Subject: "Batch created",
+          Message: JSON.stringify(productsToCreate),
+        })
+        .promise();
 
-			getSns().publish(
-				{
-					TopicArn: topicArn,
-					Subject: "Batch created",
-					Message: JSON.stringify(productsToCreate),
-				},
-				(error) => {
-					if (!error) {
-						logger.log({
-							message: `message successfully sent for batch: ${message}`,
-						});
-						return;
-					}
+				return {
+					statusCode: 201,
+					body: "Products created",
+				};
+    } catch (error) {
+      getLogger().log({
+        message: `ERROR: ${error.message}, ${JSON.stringify(error)}`,
+      });
+			return {
+				statusCode: 500,
+				body: error.message,
+			};
+    }
+  };
 
-					logger.log({
-						error: `ERROR sending email: ${error.message}, ${JSON.stringify(
-							error
-						)}`,
-					});
-				}
-			);
-		} catch (error) {
-			getLogger().log({
-				message: `ERROR: ${error.message}, ${JSON.stringify(error)}`,
-			});
-		}
-	};
-
-	return createProduct;
+  return createProduct;
 }
